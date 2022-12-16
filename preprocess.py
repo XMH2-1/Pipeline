9924b7a10b30d3975ecbd596a03b2db7b04a724c
Dragonborn <78375175+MLDovakin@users.noreply.github.com>
2022-12-16 08:26:00 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,13 @@
 import re
 
 
+
+def ReplaceThreeOrMore(s):
+    # pattern to look for three or more repetitions of any character, including
+    # newlines.
+    pattern = re.compile(r"(.)\1{2,}", re.DOTALL) 
+    return pattern.sub(r"\1\1", s)
+    
 def get_df(text_file_path, cls_name):
   print(text_file_path)
 
